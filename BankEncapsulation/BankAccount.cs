a35f627ab4264d292a7eb61f16f1aca62538b3d6
Mohammad Jabir <mohammadj94@gmail.com>
2024-06-12 11:17:58 -0600

    exercise complete

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+        private string Pin { get; set; }
+
+        public void Welcome()
+        {
+            Console.WriteLine("Welcome to your bank!");
+            do
+            {
+                Console.Write("To create an account, please enter a 4-character pin: ");
+                Pin = Console.ReadLine();
+                if (Pin.Length != 4)
+                {
+                    Console.WriteLine("Invalid pin. Please try again.");
+                }
+            } while (Pin.Length != 4);
+        }
+
+        public int MenuOptions()
+        {
+            Console.Clear();
+            Console.WriteLine("1. Deposit\n2. Withdraw\n3. Check Balance\n4. Exit\n");
+            int choice = 0;
+            do
+            {
+                Console.Write("Enter your choice: ");
+            } while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 4);
+            return choice;
+        }
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+            Console.WriteLine("Money deposited.");
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (balance >= amount)
+            {
+                balance -= amount;
+                Console.WriteLine("Money withdrawn.");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds.");
+            }
+        }
+
+        public bool ValidatePin(string pin)
+        {
+            return Pin == pin;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,76 @@
-﻿namespace BankEncapsulation
+﻿using System.Diagnostics.Metrics;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+            bool isRunning = true;
+
+            account.Welcome();
+
+            while (isRunning)
+            {
+                switch (account.MenuOptions())
+                {
+                    case 1:
+                        if (account.ValidatePin(GetUserInputForPin("Enter pin: ")))
+                        {
+                            account.Deposit(GetUserInputForFunds("Enter an amount to deposit: "));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid pin.");
+                        }
+                        break;
+                    case 2:
+                        if (account.ValidatePin(GetUserInputForPin("Enter pin: ")))
+                        {
+                            account.Withdraw(GetUserInputForFunds("Enter an amount to withdraw: "));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid pin.");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine($"Balance: {account.GetBalance():C2}");
+                        break;
+                    case 4:
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+                Console.Write("Enter any key to continue: ");
+                Console.ReadKey();
+            }
+        }
+
+        public static double GetUserInputForFunds(string str)
+        {
+            double userInput = 0;
+            do
+            {
+                Console.Write(str);
+            } while (!double.TryParse(Console.ReadLine(), out userInput) || userInput < 0);
+
+            return userInput;
+        }
+
+        public static string GetUserInputForPin(string str)
+        {
+            string userInput = "";
+            do
+            {
+                Console.Write(str);
+                userInput = Console.ReadLine();
+            } while (userInput.Length != 4);
+
+            return userInput;
         }
     }
 }
